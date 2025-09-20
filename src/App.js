40761061829ec0466fdf1df7830d16d010aa818d
steven <steven.joo@live.ca>
2025-09-19 17:30:37 -0700

    add logo and nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.scss';
 import Header from './components/Header/Header';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
 
 function App() {
   return (
-    <Header>
-
-    </Header>
+ <Header/>
+    
+    
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,35 @@
 import React from 'react'
-
+import logo from '../../assets/logosvg.svg'
 import './Header.scss'
+import { Link } from 'react-router-dom'
 
 
 export default function Header(){
     return(
-        <header>
-            hello
+        <header className='header'>
+            <nav className='nav-bar'>
+                <section className='nav-bar__heading-container'>
+                   
+                        <img
+                        src ={logo}
+                        className= 'nav-bar__heading'
+                        />
+                    
+                </section>
+                <section className='nav-bar__lists-container'>
+                    <ul className='nav-bar__lists'>
+                        <li className='nav-bar__summary'>
+                            Summary
+                        </li>
+                        <li className='nav-bar__exp'>
+                            Experience
+                        </li>
+                        <li className='nav-bar__contact'>
+                            Contact Me
+                        </li >
+                    </ul>
+                </section>
+            </nav>
         </header>
     )
 }
