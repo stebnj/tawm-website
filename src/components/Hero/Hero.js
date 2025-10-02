a40a54890f93caf0cee1937553c68b0bf38ecd07
steven <steven.joo@live.ca>
2025-10-02 03:02:26 -0700

    add fade in to summary

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,11 +2,27 @@ import React from 'react'
 import './Hero.scss'
 import wheel from '../../assets/pictures/wheel.svg'
 import college from '../../assets/pictures/college.svg'
+import {useEffect, useState } from "react"
 
 export default function Hero(){
+
+    const [showSummary, setShowSummary] = useState(false);
+    const [showLogos, setShowLogos] = useState(false)
+
+    useEffect(() => {
+        setShowSummary(true);
+
+        const timer = setTimeout(() => {
+            setShowLogos(true);
+        }, 2000)
+
+        return () => clearTimeout(timer)
+    }, [])
+
     return(
+
         <section className='hero'>
-            <div className='hero__inner-container'>
+            <div className={`hero__inner-container ${showSummary ? "fade-in" : ""}`}>
                 <article className='hero__heading'>
                     <h1 className='hero__heading-text'>
                         Summary
@@ -14,11 +30,11 @@ export default function Hero(){
                 </article>
                 <article className='hero__paragraph'>
                     <p className='hero__paragraph-text'>
-        Detail oriented and creative Mechanical Designer with 6 years of experience in 3D modeling and mechanical design, primarily using SolidWorks and AutoCAD.
-        Known for delivering high quality, manufacturable designs with a strong focus on accuracy and efficiency. 
-        Born and raised in Southern California, I bring a strong work ethic and a collaborative mindset to every project. 
-        I excel in creating detailed 3D models and assemblies that meet both functional and aesthetic requirements. 
-        Always eager to learn and adapt, I'm committed to continuous growth, whether that's mastering new CAD tools or exploring innovative design approaches to improve product performance and development cycles.
+                        Detail oriented and creative Mechanical Designer with 6 years of experience in 3D modeling and mechanical design, primarily using SolidWorks and AutoCAD.
+                        Known for delivering high quality, manufacturable designs with a strong focus on accuracy and efficiency. 
+                        Born and raised in Southern California, I bring a strong work ethic and a collaborative mindset to every project. 
+                        I excel in creating detailed 3D models and assemblies that meet both functional and aesthetic requirements. 
+                        Always eager to learn and adapt, I'm committed to continuous growth, whether that's mastering new CAD tools or exploring innovative design approaches to improve product performance and development cycles.
                     </p>
                 </article>
             </div>
@@ -29,7 +45,7 @@ export default function Hero(){
                 />
             </section>
 
-            <section className='hero__logos'>
+            <section className={`hero__logos ${showLogos ? "fade-in" : ""}`}>
                 <img 
                 className='hero__logos-college'
                 src={college}
