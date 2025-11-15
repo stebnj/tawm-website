8a3ee1cc3e0f88b9aeb545512bfb4852f7dfe402
steven <steven.joo@live.ca>
2025-11-15 15:07:24 -0800

    add text to egad exp

--- src/pages/Experience/Experience.js
+++ src/pages/Experience/Experience.js
@@ -13,15 +13,18 @@ export default function Experience(){
             </h1>
          </section>
          <section className="exp__egad">
-            <a href="https://egads.com/" className="exp__egad-link">
-              <img 
-                src= {egad}
-                className="exp__egad-img"
-              /> 
-            </a>
+          <a href="https://egads.com/" className="exp__egad-link">
+            <img 
+              src= {egad}
+              className="exp__egad-img"
+            /> 
+          </a>
       
-          <p>
-            
+          <p className="exp__egad-text">
+            In addition to performing my core responsibilities at a high level, I delivered several key accomplishments during my time at EGADS.
+             I standardized and formalized the company’s manufacturing drawing requirements, improving accuracy and reducing revision errors across projects. 
+             I consistently produced 4–5 custom signs per month while meeting tight deadlines and maintaining exceptional quality. 
+             I also contributed to a highly successful year of sales and production by helping the team manage increased project volume without compromising standards.
           </p>
          </section>
         </main>
