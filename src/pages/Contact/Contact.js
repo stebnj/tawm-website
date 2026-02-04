6f0fb2f5e267bb03341bc0bba7f54605e6c0f6d9
steven <steven.joo@live.ca>
2026-02-03 20:42:41 -0800

    add resume to link

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,4 +1,5 @@
 import './Contact.scss'
+import resume from '../../assets/resume.pdf'
 
 
 export default function Contact(){
@@ -19,10 +20,16 @@ export default function Contact(){
             <footer className='contact__content'>
                 <ul className='contact__content-list'>
                     <li className='contact__content-resume'>
-                        View My Resume
+                        <a href='/resume.pdf' target="_blank" rel="noopener noreferrer">
+                            View My Resume
+                        </a>
+                        
                     </li>
                     <li className='contact__content-linkedin'>
-                        LinkedIn
+                        <a href='https://www.linkedin.com/in/thomas-duran-iii-152400193/'>
+                            LinkedIn 
+                        </a>
+                        
                     </li>
                     <li className='contact__content-email'>
                         My Email
